b02fe89249bd2e68a03a49bccc6fbbd03998c9c5
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2026-05-24 01:25:03 +0530

    server created

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send("Api working")
+})
+
+app.listen(5001,()=>{
+    console.log("Server running at port 5001")
+})

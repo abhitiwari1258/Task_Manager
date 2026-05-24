4c606d6ce32f2a0b5a3578ffea1e41e7e769adb3
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2026-05-24 14:03:55 +0530

    controller crt

---
+++ server/controllers/authController.js
@@ -0,0 +1,34 @@
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+
+const registerUser = async(req,res)=>{
+    try{
+        const {name,email,password} = req.body
+        const userExists = await User.findOne({email})
+
+        if(userExists){
+            return res.status(400).json({
+                message: "User already exist"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({
+            message: "User registered",
+            user
+        });
+
+    }catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+}

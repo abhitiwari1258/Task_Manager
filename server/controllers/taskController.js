4c039d403adb172164543d0cd8e92189084e03a2
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2026-05-26 00:54:37 +0530

    create get update delete task created testing done

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,8 +1,8 @@
 const Task = require('../models/task')
 
-const createTask = async(req,res)=>{
-    try{
-        const {title, description,priority} = req.body
+const createTask = async (req, res) => {
+    try {
+        const { title, description, priority } = req.body
 
         const task = await Task.create({
             title,
@@ -11,13 +11,74 @@ const createTask = async(req,res)=>{
             user: req.user._id
         })
 
-        res.status(201).json({task})
+        res.status(201).json({ task })
 
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
-            message:error.message
+            message: error.message
         });
     }
 }
 
-module.exports = {createTask}
+const getTasks = async (req, res) => {
+    try {
+        const task = await Task.find({
+            user: req.user._id
+        })
+
+        res.json(task)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+const updateTask = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id)
+        console.log(task.user.toString())
+        console.log(req.user._id.toString())
+
+        if (!task) {
+            return res.status(404).json({
+                message: "Task not found"
+            });
+        }
+
+        if (task.user.toString() !== req.user._id.toString()) {
+            return res.status(401).json({
+                message: "Not authorized"
+            });
+        }
+
+        const updated = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+        res.json(updated);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+const deleteTask = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+
+        if (!task) {
+            return res.status(404).json({
+                message: "Task not found"
+            });
+        }
+
+        await task.deleteOne();
+        res.json({message:"Task deleted"});
+
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+
+}
+module.exports = { createTask, getTasks, updateTask, deleteTask }
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const protectRoutes = require('../middleware/authMiddleware')
-const {createTask} = require('../controllers/taskController')
+const {createTask, getTasks, updateTask, deleteTask} = require('../controllers/taskController')
 const router = express.Router()
 
 router.post('/',protectRoutes,createTask)
+router.get('/',protectRoutes,getTasks)
+router.put('/:id',protectRoutes,updateTask)
+router.delete('/:id',protectRoutes,deleteTask)
 
 module.exports = router;

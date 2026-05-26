51cc7e648e084fa777ec3cf3e3e0b11b1d622c67
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2026-05-26 17:14:34 +0530

    login working

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
-
+import {useState} from 'react'
+import API from '../services/api'
+import {useNavigate, useNavigation} from 'react-router-dom'
 const Login = () => {
+    const navigate = useNavigate()
+    const [form,setForm] = useState({
+        email:'',
+        password:''
+    })
+
+    const handleChange = (e)=>{
+        setForm({...form, [e.target.name] : e.target.value })
+    }
+
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
+
+        try{
+            const res = await API.post("/auth/login",form)
+
+            localStorage.setItem("token",res.data.token)
+
+            navigate("/dashboard")
+        }catch(err){
+            console.log(err.response?.data ||err.message);
+        }
+    }
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="text"
+      name="email"
+      placeholder='email'
+      onChange={handleChange}
+      />
+
+      <input 
+      type="password"
+      name="password"
+      placeholder='password'
+      onChange={handleChange}
+      />
+
+      <button>Login</button>
+    </form>
   )
 }
 

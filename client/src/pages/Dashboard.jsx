862968fa4ed61f2e5d0e2ae44934f1f1b1e775f8
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2026-05-27 16:41:54 +0530

    frontend: crt task done

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from 'react'
 import API from '../services/api'
 const Dashboard = () => {
   const [tasks,setTasks] = useState([])
-  // console.log(tasks)
+  const [form,setForms] = useState({
+    title:"",
+    description:"",
+    priority:"Low"
+  })
+
+  // console.log(form)
 
   useEffect(()=>{
     fetchTasks();
@@ -21,9 +27,63 @@ const Dashboard = () => {
       console.log(error.response?.data)
     }
   }
+
+  const handleChange = (e)=>{
+    setForms({...form, [e.target.name] : e.target.value})
+  }
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+
+    try{
+      const token = localStorage.getItem("token")
+
+      await API.post('/task', form, {headers:{Authorization: `Bearer ${token}`}})
+
+      setForms({
+        title:"",
+        description:"",
+        priority:"Low"
+      })
+
+      fetchTasks()
+
+    }catch(error){
+      console.log(error.response?.data);
+    }
+  }
   return (
     <div>
       <h1>Dashboard</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+        name='title'
+        placeholder="Title"
+        value={form.title}
+        onChange={handleChange}
+        />
+
+        <input type="text"
+        name='description'
+        placeholder="Description"
+        value={form.description}
+        onChange={handleChange}
+        />
+
+        <select name="priority"
+        value={form.priority}
+        onChange={handleChange}
+        >
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="High">High</option>
+        </select>
+
+        <button>Add Task</button>
+      </form>
+      <hr />
+
       <h2>My Task</h2>
 
       {tasks.map((task)=>
